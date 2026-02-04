2bd19057c4ad177be4ee7f55b8dbf463eb23b52b
pratyush06-aec <pratyush00710@gmail.com>
2026-02-04 18:24:04 +0530

    updated some leftovers

--- Hello/Hello/settings.py
+++ Hello/Hello/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#w09!3zu3!3l1#(#&j599gxu54_tsz(@m&qm=u6ufos_2kgt$m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
